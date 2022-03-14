1ecc54de4c5eefa8cea9e9ff16247d91aca04f63
Sergey Zhiltsov <sergey.zhiltsov@sigma.software>
2022-03-15 00:26:17 +0200

    Remove extra logs redirection

--- run.sh
+++ run.sh
@@ -3,6 +3,6 @@
 function run() {
   docker-compose run --rm bot $1
 }
-run ./duco_report.py >> ./logs.log
-run ./xmr_mine_report.py >> ./logs.log
-run ./xmr_nano_report.py >> ./logs.log
+run ./duco_report.py
+run ./xmr_mine_report.py
+run ./xmr_nano_report.py
